5bdcf302240284025d6e6f75e171674ce6d0d2b0
Aidar Putilov <putiloff@gmail.com>
2024-05-13 02:22:33 +0500

    Modify test_operation.py

--- test/test_operation.py
+++ test/test_operation.py
@@ -1,24 +1,31 @@
+import pytest
 from src.operation import Operation
 
 
-def test_operation_instance():
+def test_operation_instance(operation_fixture):
+    """ Функция проверки создания экземпляра Operation """
 
-    op = Operation(
-        pk=587085106,
-        state="EXECUTED",
-        date_time="2018-03-23T10:45:06.972075",
-        amount="48223.05",
-        currency="руб.",
-        description="Открытие вклада",
-        from_="Счет 41421565395219882431",
-        to="Visa Classic 6831982476737658"
-    )
+    assert operation_fixture[0].from_type == "Счет"
+    assert operation_fixture[0].from_number == "41421565395219882431"
+    assert operation_fixture[0].from_number_encript == "**2431"
+    assert operation_fixture[0].to_type == "Visa Classic"
+    assert operation_fixture[0].to_number == "6831982476737658"
+    assert operation_fixture[0].to_number_encript == "6831 98** **** 7658"
+    assert operation_fixture[0].date == "23.03.2018"
 
-    assert op.from_type == "Счет"
-    assert op.from_number == "41421565395219882431"
-    assert op.from_number_encript == "**2431"
-    assert op.to_type == "Visa Classic"
-    assert op.to_number == "6831982476737658"
-    assert op.to_number_encript == "6831 98** **** 7658"
-    assert op.date == "23.03.2018"
+    # Отсутствует поле from
+    assert operation_fixture[1].from_type == ""
+    assert operation_fixture[1].from_number == ""
+    assert operation_fixture[1].from_number_encript == ""
 
+def test_operation_status(operation_fixture):
+    """ Проверка вывода стутуса операции """
+
+    assert operation_fixture[0].get_status() == "23.03.2018 Открытие вклада\n" \
+            "Счет **2431 -> Visa Classic 6831 98** **** 7658\n" \
+            "48223.05 руб.\n"
+
+    # Отсутствует поле from
+    assert operation_fixture[1].get_status() == "18.07.2019 Перевод с карты на карту\n" \
+            "  -> МИР 2956 60** **** 3342\n" \
+            "82139.20 руб.\n"
