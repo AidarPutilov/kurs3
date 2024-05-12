24714e13216cb882da6be1ac4ea2bb94fe6e16c8
Aidar Putilov <putiloff@gmail.com>
2024-05-13 04:10:42 +0500

    Modify conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -4,23 +4,23 @@ from src.operation import Operation
 
 
 @pytest.fixture
-def operation_fixture():
+def operation_class_fixture():
     return [Operation(
-            pk=587085106,
-            state="EXECUTED",
-            date_time="2018-03-23T10:45:06.972075",
-            amount="48223.05",
-            currency="руб.",
-            description="Открытие вклада",
-            from_="Счет 41421565395219882431",
-            to="Visa Classic 6831982476737658"
-        ),
-        Operation(
-            pk=917824439,
-            state="EXECUTED",
-            date_time="2019-07-18T12:27:13.355343",
-            amount="82139.20",
-            currency="руб.",
-            description="Перевод с карты на карту",
-            to="МИР 2956603572573342"
-        )]
+                pk=587085106,
+                state="EXECUTED",
+                date_time="2018-03-23T10:45:06.972075",
+                amount="48223.05",
+                currency="руб.",
+                description="Открытие вклада",
+                to="Visa Classic 6831982476737658",
+                from_="Счет 41421565395219882431"
+            ),
+            Operation(
+                pk=917824439,
+                state="EXECUTED",
+                date_time="2019-07-18T12:27:13.355343",
+                amount="82139.20",
+                currency="руб.",
+                description="Перевод с карты на карту",
+                to="МИР 2956603572573342"
+            )]
