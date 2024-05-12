cee1084709057a0a86c5e83407c65c5c37ab16f7
Aidar Putilov <putiloff@gmail.com>
2024-05-13 04:11:20 +0500

    Modify main.py

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ from src.functions import (load_json,
 
 
 def main():
-    operations = load_json(DATA_FILE_PATH)
-    executed_operations = get_executed_operations(operations)
-    openation_instances = get_operation_instances(executed_operations)
-    sort_operations = sort_operations_by_date(openation_instances)
 
+    # Чтение данных из файла JSON
+    operations = load_json(DATA_FILE_PATH)
+
+    # Выборка операций со статусом 'EXECUTED'
+    executed_operations = get_executed_operations(operations)
+
+    # Создание экземпляров класса
+    operation_instances = get_operation_instances(executed_operations)
+
+    # Сортировка по дате
+    sort_operations = sort_operations_by_date(operation_instances)
+
+    # Вывод статусов операций
     [print(op.get_status()) for op in sort_operations[:5]]
 
 
